36ed6ab1b5006ab4e40069267c0aeb7b9a3adc61
JCBenR <jsieb81@gmail.com>
2021-08-10 20:17:48 -0600

    first commit

---
+++ lightapp.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $("#d1").show();
+//     $("#d2").hide();
+//     $("span").click(function(){ // For demo's sake we attach the crossFade to a click event.
+//       $("#d1").fadeToggle(5);
+//       $("#d2").fadeToggle(5);
+//     });
+//   });
